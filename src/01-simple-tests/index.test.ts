5e6ad7c1f491f87507478d330826caef8c247e0c
nayki1144 <narek1144@gmail.com>
2023-06-29 22:27:56 +0400

    feat: done01-simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,69 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 5,
+        b: 8,
+        action: Action.Add,
+      }),
+    ).toBe(13);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 10,
+        b: 8,
+        action: Action.Subtract,
+      }),
+    ).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 10,
+        b: 8,
+        action: Action.Multiply,
+      }),
+    ).toBe(80);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 10,
+        b: 2,
+        action: Action.Divide,
+      }),
+    ).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 10,
+        b: 2,
+        action: Action.Exponentiate,
+      }),
+    ).toBe(100);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const obj = Object.values(Action);
+    expect(obj).toEqual(['+', '-', '/', '*', '^']);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 10,
+        b: 2,
+        action: Action.Divide,
+      }),
+    ).not.toBeNull();
   });
 });
